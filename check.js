6f28d4aeac81facd1dce08317e3b231a6f6f6eee
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-02 10:04:41 -0400

    adds basic error handling and passing tests

--- check.js
+++ check.js
@@ -1,5 +1,12 @@
 const check = (number) => {
-  if (!number) throw new Error("check() requires a number to be provided as the argument");
+  const inValidArg = "check() requires 1 argument: the number to be checked";
+  const error = (message) => new Error(message);
+
+
+  if (!number) throw error(inValidArg);
+
+  if (arguments.length > 1) throw error(inValidArg);
+
   return;
 };
 
--- test/checkTest.js
+++ test/checkTest.js
@@ -5,7 +5,11 @@ const check = require('../check');
 
 describe('check', function() {
   it('should throw an error if passed no arguments', function() {
-    const expectedOutput = "check() requires a number to be provided as the argument";
+    const expectedOutput = "check() requires 1 argument: the number to be checked";
     assert.throws(() => check(), expectedOutput);
+  });
+  it('should throw an error if passed more than 1 argument', function() {
+    const expectedOutput = "check() requires 1 argument: the number to be checked";
+    assert.throws(() => check(1, 2), expectedOutput);
   });
 });
